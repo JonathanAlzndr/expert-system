df307acc453f7bf0edeee75357176fcd12e27f9b
marcoismakalew <marcoismakalew@gmail.com>
2025-10-31 22:08:03 +0800

    make: diagnosis questions

--- fe/src/components/Button.jsx
+++ fe/src/components/Button.jsx
@@ -1,19 +1,18 @@
-export default function Button({ text, onClick, color = "green" }) {
-  let variant = "";
+export default function Button({ text, onClick, color = 'green' }) {
+  let variant = '';
   switch (color) {
-    case "blue":
-      variant = "bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white";
+    case 'blue':
+      variant = 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white';
       break;
-    case "red":
-      variant = "bg-red-600 hover:bg-red-700 active:bg-red-800 text-white";
+    case 'red':
+      variant = 'bg-red-700/90 hover:bg-red-700 active:bg-red-800 text-white';
       break;
-    case "secondRed":
+    case 'secondRed':
       variant =
-        "bg-merahTua2 font-light hover:bg-red-500 active:bg-red-900 active:text-red-900 text-red-500 hover:text-red-900";
+        'bg-merahTua2 font-light hover:bg-red-500 active:bg-red-900 active:text-red-900 text-red-500 hover:text-red-900';
       break;
     default:
-      variant =
-        "bg-green-600 hover:bg-green-700 active:bg-green-800 text-white";
+      variant = 'bg-green-600 hover:bg-green-700 active:bg-green-800 text-white';
   }
   return (
     <button
