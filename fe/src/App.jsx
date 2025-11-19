1b01585bb63168e22470c8fdee62841068a9ce3d
marcoismakalew <marcoismakalew@gmail.com>
2025-11-20 00:54:26 +0800

    make: route

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+/* layouts */
 import Layout from "./layouts/Layout";
 import LayoutAdmin from "./layouts/LayoutAdmin";
+/* components */
+import PrivateRoute from "./components/PrivateRoute";
+/* pages */
+import DashboardAdmin from "./pages/DashboardAdmin";
+import Diagnosis from "./pages/Diagnosis";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
-import DashboardAdmin from "./pages/DashboardAdmin";
-import PrivateRoute from "./components/PrivateRoute";
-import Diagnosis from "./pages/Diagnosis";
+import DiagnosisResults from "./pages/DiagnosisResults";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
         <Route path="diagnosis" element={<Diagnosis />} />
+        <Route path="results" element={<DiagnosisResults />} />
       </Route>
 
       <Route path="/" element={<LayoutAdmin />}>
