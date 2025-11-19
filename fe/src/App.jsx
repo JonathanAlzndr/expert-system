a513d02af325119107964f247453769014aa4471
marcoismakalew <marcoismakalew@gmail.com>
2025-11-20 00:35:51 +0800

    nothing

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,29 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout"; // Layout untuk halaman umum
-import LayoutAdmin from "./layouts/LayoutAdmin"; // Layout khusus admin
+import Layout from "./layouts/Layout";
+import LayoutAdmin from "./layouts/LayoutAdmin";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
-import DashboardAdmin from "./pages/DashboardAdmin"; // Halaman admin
-import PrivateRoute from "./components/PrivateRoute"; // Import PrivateRoute
+import DashboardAdmin from "./pages/DashboardAdmin";
+import PrivateRoute from "./components/PrivateRoute";
 import Diagnosis from "./pages/Diagnosis";
 
 function App() {
   return (
     <Routes>
-      {/* Layout umum untuk halaman public */}
       <Route path="/" element={<Layout />}>
         <Route index element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="diagnosis" element={<Diagnosis />} />
       </Route>
 
-      {/* LayoutAdmin untuk halaman admin, dengan pengecekan token */}
       <Route path="/" element={<LayoutAdmin />}>
-        {/* Gunakan PrivateRoute untuk memastikan hanya admin yang login yang bisa mengakses /admin */}
         <Route
           path="/admin"
-          element={<PrivateRoute element={<DashboardAdmin />} />} // Berikan komponen dengan element
+          element={<PrivateRoute element={<DashboardAdmin />} />}
         />
-        <Route path="diagnosis" element={<Diagnosis />} />
       </Route>
     </Routes>
   );
